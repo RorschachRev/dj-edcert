80ac2f23a3d2155bd0eeb39bfe5fa21dfa4d95e7
Alex <alexheistdev@gmail.com>
2019-01-16 12:51:30 -0700

    final frame push

--- wwwdj/views.py
+++ wwwdj/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from wwwdj.eth import BC
 
 def index(request):
 	return render(request, 'pages/index.html')
+
+# Example Blockchain Query View for Templates
+def get_block_number(request):
+	w3 = BC().resp.w3
+	block_number = w3.eth.blockNumber
+	context = {
+		'block_number': block_number,
+	}
+	return render(request, 'pages/index.html', context)
